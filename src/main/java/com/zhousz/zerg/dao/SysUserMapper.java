46db0c53d19ce7c453fcd15b8b891641ea3dc965
zhousz <13687646803@163.com>
2018-11-07 17:12:00 +0800

    多数据源配置 2018年11月7日17:11:46

--- src/main/java/com/zhousz/zerg/dao/SysUserMapper.java
+++ src/main/java/com/zhousz/zerg/dao/SysUserMapper.java
@@ -3,6 +3,8 @@ package com.zhousz.zerg.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +14,11 @@ import com.zhousz.zerg.domain.SysUser;
 @Mapper
 public interface SysUserMapper {
 
-	@Select("select * from sys_user")
+	@Select("select oper_Id, emp_Name from sys_user")
+	@Results(id = "sysUser", value = {
+	@Result(property = "operId", column = "oper_id", javaType = String.class),
+	@Result(property = "empName", column = "emp_name", javaType = String.class)
+	})
 	List<SysUser> querySysUsers();
 	
 }
